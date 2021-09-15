df5bc9ef0c36a69abc6f5e2aa15abd2a0ae827e0
DS Andres Ayala <jorgi710@hotmail.com>
2021-09-15 15:45:01 -0500

    Finalizamos el entender por que usar Numpy y para que se usa Pandas Luego vamos a trabajar con otro sed de datos

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# Explorar, analizar y visualizar los datos
+
+data = [50, 50, 47, 97, 49, 3, 53, 42, 26, 74, 82, 62, 37, 15, 70, 27, 36, 35, 48, 52, 63, 64]
+# print(data)
+grades = np.array(data)
+print(type(data), "*2", data*2)
+print(type(grades), "*2", grades*2)
+
+
+
+
+
+
+
+
+
